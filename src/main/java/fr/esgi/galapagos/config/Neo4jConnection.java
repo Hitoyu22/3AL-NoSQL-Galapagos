138a3e546c7e9c4fe46a682a75101b94b373f264
Rémy <remy.thibaut2005@gmail.com>
2025-11-02 14:17:48 +0100

    Create Neo4jConnection

---
+++ src/main/java/fr/esgi/galapagos/config/Neo4jConnection.java
@@ -0,0 +1,29 @@
+package fr.esgi.galapagos.config;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+
+public class Neo4jConnection {
+
+    private static Driver driver;
+
+    private Neo4jConnection() {
+    }
+
+    public static synchronized void getDriver() {
+        if (driver == null) {
+            String uri = DatabaseConfig.getNeo4jUri();
+            String user = DatabaseConfig.getNeo4jUser();
+            String password = DatabaseConfig.getNeo4jPassword();
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+        }
+    }
+
+    public static void close() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+    }
+}
