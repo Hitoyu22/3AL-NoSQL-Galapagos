ef851f1dc34b3a3eb5a71df5ff63843fed5bdea9
Rémy THIBAUT <remy.thibaut2005@gmail.com>
2025-11-27 23:15:34 +0100

    Create First Pages

---
+++ frontend/app/error.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{
+  error: NuxtError
+}>()
+
+useHead({
+  title: 'Une erreur est survenue - Galapagos',
+  htmlAttrs: {
+    lang: 'fr'
+  }
+})
+</script>
+
+<template>
+  <UApp>
+    <div class="min-h-screen flex flex-col">
+        <NavBar /> 
+        <UContainer class="flex items-center justify-center">
+        <UError 
+          :error="error.statusCode === 404 ? {
+            statusCode: 404,
+            statusMessage: 'Page non trouvée',
+            message: 'La page que vous recherchez n\'existe pas.'
+          } : error"
+          :clear="{
+            label : 'Retour à l\'accueil'
+          }"
+        />
+      </UContainer>
+    </div>
+  </UApp>
+</template>
---
+++ frontend/app/layouts/DefaultLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NavBar/>
+    <UContainer class="py-8">
+      <slot />
+    </UContainer>
+  </div>
+</template>
